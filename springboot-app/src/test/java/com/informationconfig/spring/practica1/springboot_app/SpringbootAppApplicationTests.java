e3ba7381812714a44afe3ca2a917bce9ac474cd1
Richard Parra Zegarra <110671604+richardparra99@users.noreply.github.com>
2025-07-23 00:18:13 -0400

    Practicando sintaxis de Spring

---
+++ springboot-app/src/test/java/com/informationconfig/spring/practica1/springboot_app/SpringbootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.informationconfig.spring.practica1.springboot_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
