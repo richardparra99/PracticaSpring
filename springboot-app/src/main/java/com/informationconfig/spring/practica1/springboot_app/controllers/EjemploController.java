b853174936f04c3291307b28006e4ce559817294
Richard Parra Zegarra <110671604+richardparra99@users.noreply.github.com>
2025-07-23 02:06:47 -0400

    se hizo modificaciones

--- springboot-app/src/main/java/com/informationconfig/spring/practica1/springboot_app/controllers/EjemploController.java
+++ springboot-app/src/main/java/com/informationconfig/spring/practica1/springboot_app/controllers/EjemploController.java
@@ -1,6 +1,7 @@
 package com.informationconfig.spring.practica1.springboot_app.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -8,8 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class EjemploController {
 
     @GetMapping("/detalles_info")
-    public String info(){
-
+    public String info(Model model){
+        model.addAttribute("Titulo", "Servidor en linea");
+        model.addAttribute("Servidor", "informationconfig");
+        model.addAttribute("ip", "192.168.1.1");
         return "detalles_info";
     }
 
